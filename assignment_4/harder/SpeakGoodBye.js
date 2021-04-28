3797ceb408f7ee5fcc9b70516107385bd6a49a70
Manolis <cartmantemfe@gmail.com>
2021-04-28 23:44:53 +0300

    Assignment Week 4 Revision

--- assignment_4/harder/SpeakGoodBye.js
+++ assignment_4/harder/SpeakGoodBye.js
@@ -18,7 +18,7 @@
   // byeSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
   byeSpeaker.speak = function(someName) {
-    console.log(speakWord + " " + someName);
+    return (speakWord + " " + someName);
   }
 
   // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
